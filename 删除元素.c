96e8f0b84d6baea61971a702da4c8fc4ddbae746
22ALAN <1497400685@qq.com>
2026-04-25 17:21:49 +0800

    按照要求处理数组中的元素

---
+++ 删除元素.c
@@ -0,0 +1,68 @@
+/*************************************************************************
+	> File Name: 删除元素.c
+	> Author: zhh
+	> Mail:1497400685@qq.com 
+	> Created Time: 六  4月/25 13:14:10 2026
+ ************************************************************************/
+
+#include<stdio.h>
+
+#include <stdlib.h>
+#ifdef __has_include
+    #if __has_include(<malloc.h>)
+        #include <malloc.h>
+    #endif
+#endif
+//一个数组内，删除一个特定的数字
+int removeElement(int* nums,int numsSize,int val)
+{
+    int src=0,dst = 0;
+    while(src<numsSize)
+    {
+        if(nums[src] != val)
+        {
+            nums[dst] = nums[src];
+            ++src;
+            dst++;
+
+        }
+        else
+        {
+            src++;
+        }
+    }
+    return dst;
+
+}
+
+
+//删除排序数组中的重复项
+
+int removeDuplicates(int* nums,int numsSize)
+{
+
+    int prev=0,cur = 1；
+    
+    while(cur<numsSize)
+    {
+        if(nums[prev] != nums[cur])
+        {
+            nums[dst] = nums[prev];
+            prev++;
+            cur++;
+            dst++;
+        }
+        else
+        {
+            prev++;
+            cur++;
+        }
+    }
+
+    nums[dst] = nums[prev];
+    dst++;
+    prev++;
+
+}
+
+
